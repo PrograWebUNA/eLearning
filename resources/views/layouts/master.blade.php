15bb4f0588dc594d6692e9070456700f575e0554
Alejandro Sanchez <rasso92@hotmail.com>
2017-03-27 23:26:21 -0600

    Carga inicial

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>e-Learning</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="{{ url('css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="{{ url('css/blog-home.css')}}" rel="stylesheet">
+
+
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    @include('partials.navbar')
+
+    <!-- Page Content -->
+    <div class="container">
+      @yield('content')
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2017</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="{{ url('js/jquery.js') }}"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{ url('js/bootstrap.min.js') }}"></script>
+
+</body>
+
+</html>
