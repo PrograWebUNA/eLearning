a2c19a0e987631a5527baadf3b0c6cb11b916b31
Juan Jara <jjherr3ra@gmail.com>
2017-04-09 21:00:49 -0600

    homeController
    funciona para mostrar cursos de un usuario en especifico en el andes en
    mostrar cursos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,6 +16,19 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
+    public function showMyCourses(){
+      $cursos = DB::table('MATRICULA')
+      ->join('CURSO', function ($join){
+          $join->on('MATRICULA.CURSO','=','CURSO.ID_CURSO');
+      })->join('users',function($join2){
+          $join2->on('MATRICULA.ID_USUARIO','=','users.ID_USUARIO');
+      })->select('CURSO.NOMBRE AS NOMBRE_CURSO','users.ID_USUARIO AS USER_ID','MATRICULA.*')->get();
+
+    //  $matriculas = Matricula::orderBy('ID_MATRICULA')->get();
+    //  $course = DB::table('curso')->get();
+     return view('content.index', compact('cursos'));
+    }
+
     /**
      * Show the application dashboard.
      *
