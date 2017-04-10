63bd44091b05a735eae304ce3833360659c96dce
Alejandro Sanchez <rasso92@hotmail.com>
2017-04-09 21:29:04 -0600

    Resgisto

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,8 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
@@ -36,7 +38,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+
     }
 
     /**
@@ -75,5 +77,13 @@ class RegisterController extends Controller
             'PAIS' => $data['PAIS'],
             'LENGUAJE' => $data['LENGUAJE'],
         ]);
+    }
+
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+        event(new Registered($user = $this->create($request->all())));
+        return $this->registered($request, $user)
+            ?: redirect($this->redirectPath());
     }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,6 +36,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('/');
+        return view('home');
     }
 }
